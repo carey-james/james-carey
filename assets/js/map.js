3be05693d482f55dbe25ee067ccfdf003ecd5e40
James Carey <57900176+carey-james@users.noreply.github.com>
2024-04-03 13:23:32 -0400

    Legend and Icon Updates

--- assets/js/map.js
+++ assets/js/map.js
@@ -21,6 +21,18 @@ async function initMap() {
       toggleHighlight(AdvancedMarkerElement, restaurant);
     });
   }
+
+  for (const key in legend_colors) {
+      const type = legend_colors[key];
+      const name = type.name;
+      const color = type.color;
+      const div = document.createElement("div");
+
+      div.innerHTML = `<i class="fa-solid fa-circle ${color}"></i>` + name;
+      legend.appendChild(div);
+    }
+    map.controls[google.maps.ControlPosition.RIGHT_BOTTOM].push(legend);
+
 }
 
 function toggleHighlight(markerView, restaurant) {
@@ -38,29 +50,29 @@ function buildContent(restaurant) {
 
   content.classList.add("restaurant");
   content.innerHTML = `
-    <div class="icon sit-down">
-        		<i class="fa-solid fa-${restaurant.icon1} fa-md"></i>
+    <div class="icon ${restaurant.type}">
+            <i class="fa-solid fa-${restaurant.icon1} fa-md"></i>
             <i class="fa-solid fa-${restaurant.icon2} fa-md"></i>
     </div>
     <div class="details">
-        <div class="price">${restaurant.name}</div>
+        <div class="name"><a href="${restaurant.link}">${restaurant.name}</a></div>
         <div class="address">${restaurant.address}</div>
         <div class="description">${restaurant.description}</div>
         <div class="features">
         <div>
-            <i aria-hidden="true" class="fa fa-bed fa-lg bed" title="bedroom"></i>
-            <span class="fa-sr-only">bedroom</span>
-            <span>${restaurant.bed}</span>
+            <i aria-hidden="true" class="fa-solid fa-badge-dollar fa-lg dollar" title="Average Dinner"></i>
+            <span class="fa-sr-only">Average Dinner</span>
+            <span>${restaurant.price}</span>
         </div>
         <div>
-            <i aria-hidden="true" class="fa fa-bath fa-lg bath" title="bathroom"></i>
-            <span class="fa-sr-only">bathroom</span>
-            <span>${restaurant.bath}</span>
+            <i aria-hidden="true" class="fa-solid fa-${restaurant.extra_icon} fa-lg ${restaurant.extra_color}" title="Extra"></i>
+            <span class="fa-sr-only">Extra</span>
+            <span>${restaurant.extra_text}</span>
         </div>
         <div>
-            <i aria-hidden="true" class="fa fa-ruler fa-lg size" title="size"></i>
-            <span class="fa-sr-only">size</span>
-            <span>${restaurant.size} ft<sup>2</sup></span>
+            <i aria-hidden="true" class="fa-solid fa-calendar-lines-pen fa-lg rez" title="Reservation"></i>
+            <span class="fa-sr-only">Reservation</span>
+            <span>${restaurant.rez}</span>
         </div>
         </div>
     </div>
@@ -68,22 +80,71 @@ function buildContent(restaurant) {
   return content;
 }
 
+const legend_colors = {
+  grab_and_go: {
+    name: "Grab & Go",
+    color: "grab-and-go"
+  },
+  affordable_sitdown: {
+    name: "Affordable Sitdown",
+    color: "affordable"
+  },
+  affordable_and_drinks: {
+    name: "Affordable & Drinks",
+    color: "affordable-and-drinks"
+  },
+  expensive_sitdown: {
+    name: "Expensive Sitdown",
+    color: "expensive"
+  },
+  expensive_and_drinks: {
+    name: "Expensive & Drinks",
+    color: "expensive-and-drinks"
+  },
+  drinks: {
+    name: "Drinks",
+    color: "drinks"
+  },
+
+}
+
 const restaurants = [
-    {
-        "name": "Astoria",
-        "address": "1521 17th St NW",
-        "description": "Chinese & Cocktails",
-        "icon1": "bowl-chopsticks-noodles",
-        "icon2": "martini-glass",
-        "building": "home",
-        "bed": "$$",
-        "bath": 4.5,
-        "size": 300,
-        "position": {
-            "lat": 38.91074373631267,
-            "lng": -77.03832824711489
-        }
-    }
+  {
+    name: "Astoria",
+    link: "https://astoriadc.com/",
+    address: "1521 17th St NW",
+    description: "Chinese & Cocktails",
+    icon1: "bowl-chopsticks-noodles",
+    icon2: "martini-glass",
+    type: "expensive-and-drinks",
+    price: "$40",
+    extra_icon: "leaf",
+    extra_color: "veg",
+    extra_text: "Vegan OK",
+    rez: "Rez Rec'd",
+    position: {
+      lat: 38.91074373631267,
+      lng: -77.03832824711489,
+    },
+  },
+  {
+    name: "Aslin DC",
+    link: "https://www.aslinbeer.com/dc",
+    address: "1740 14th St NW",
+    description: "Beergarden",
+    icon1: "beer-mug",
+    icon2: "",
+    type: "drinks",
+    price: "$8",
+    extra_icon: "utensils",
+    extra_color: "veg",
+    extra_text: "Snacks",
+    rez: "No Rez",
+    position: {
+      lat: 38.91394036088174,
+      lng: -77.03242493027032,
+    },
+  },
 ];
 
 initMap();
