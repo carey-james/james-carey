f166826844168e8d20e6c8fa2abc1919a3626490
James Carey <57900176+carey-james@users.noreply.github.com>
2024-05-14 18:42:17 -0400

    Added Birthday RSVP

--- assets/js/books.js
+++ assets/js/books.js
@@ -6,5 +6,6 @@ async function initGraphs() {
 	const data_2023 = await d3.dsv("|", "https://raw.githubusercontent.com/carey-james/Reading-List/main/2023/books.csv");
 	const data_2024 = await d3.dsv("|", "https://raw.githubusercontent.com/carey-james/Reading-List/main/2024/books.csv");
 
-	
+	// 
+	// Set 
 }
