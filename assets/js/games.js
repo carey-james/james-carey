c4283bffb35c0aa9e18358446c6e3a16c6f9f4f0
James Carey <57900176+carey-james@users.noreply.github.com>
2024-12-19 10:23:51 -0800

    Trying AG Gird for Games

--- assets/js/games.js
+++ assets/js/games.js
@@ -1,5 +1,28 @@
 'use strict';
 
+function runner(games_data) {
+	let gridApi;
+
+	// Grid Options: Contains all of the Data Grid configurations
+	const gridOptions = {
+		rowData: games_data,
+		columnDefs: [
+			{ field: 'game' },
+			{ field: 'mechanics' },
+			{ field: 'theme' },
+			{ field: 'min_players' },
+			{ field: 'max_players' }
+		],
+		defaultColDef: {
+    		flex: 1,
+  		},
+	};
+
+	// Your Javascript code to create the Data Grid
+	const myGridElement = document.querySelector('#gamesGrid');
+	agGrid.createGrid(myGridElement, gridOptions);
+}
+
 async function initGames() {
 	// Get the games from the Games List repo
 	// https://github.com/carey-james/Games-List
