177874f74a9befe4b312929412c408c5f47e55b0
James Carey <57900176+carey-james@users.noreply.github.com>
2024-12-19 09:22:02 -0800

    Starting Games Page

--- assets/js/books.js
+++ assets/js/books.js
@@ -608,9 +608,9 @@ function runner(book_data) {
 async function initBooks() {
 	// Get the books data from the Reading List repo
 	// https://github.com/carey-james/Reading-List
-	const data_2022 = await d3.dsv("|", "https://raw.githubusercontent.com/carey-james/Reading-List/main/2022/books.csv");
-	const data_2023 = await d3.dsv("|", "https://raw.githubusercontent.com/carey-james/Reading-List/main/2023/books.csv");
-	const data_2024 = await d3.dsv("|", "https://raw.githubusercontent.com/carey-james/Reading-List/main/2024/books.csv");
+	const data_2022 = await d3.dsv('|', 'https://raw.githubusercontent.com/carey-james/Reading-List/main/2022/books.csv');
+	const data_2023 = await d3.dsv('|', 'https://raw.githubusercontent.com/carey-james/Reading-List/main/2023/books.csv');
+	const data_2024 = await d3.dsv('|', 'https://raw.githubusercontent.com/carey-james/Reading-List/main/2024/books.csv');
 	const raw_data = data_2022.concat(data_2023.concat(data_2024));
 	const data = _.sortBy(raw_data.map(dateFixer),['year','date']);
 	runner(data);
---
+++ assets/js/games.js
@@ -0,0 +1,7 @@
+'use strict';
+
+async function initGames() {
+	// Get the games from the Games List repo
+	// https://github.com/carey-james/Games-List
+	const games = await d3.dsv('|', 'https://raw.githubusercontent.com/carey-james/Games-List/refs/heads/main/games.csv');
+}
