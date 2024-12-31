5c81a6beb4787d971072d8aaf4d37824bdf976fb
James Carey <57900176+carey-james@users.noreply.github.com>
2024-12-31 13:35:34 -0500

    Update books.js

--- assets/js/books.js
+++ assets/js/books.js
@@ -320,7 +320,7 @@ function runner(book_data) {
         		d3.select(`#legend-0-${labelCounts[0] - 1}`).text(counts[0]);
         		counts[2] = counts[0]; // Used for Percentage
         		runningCounts.forEach(function(value, key){
-        			d3.select(`#legend-1-percent-${key}`).text(`${((value / counts[0]) * 100).toString().split('.')[0]}%`);
+        			d3.select(`#legend-1-percent-${key - 1}`).text(`${((value / counts[0]) * 100).toString().split('.')[0]}%`);
         		})
         		runningCounts.clear();
        	 		counts[0] = 0;
@@ -338,6 +338,10 @@ function runner(book_data) {
       		if (i === sortedBooks.length - 1) {
         		d3.select(`#legend-0-${labelCounts[0] - 1}`).text(counts[0] + 1);
         		d3.select(`#legend-1-${labelCounts[1] - 1}`).text(counts[1] + 1);
+        		runningCounts.forEach(function(value, key){
+        			d3.select(`#legend-1-percent-${key - 1}`).text(`${((value / counts[0]) * 100).toString().split('.')[0]}%`);
+        		})
+        		runningCounts.clear();
       		}
       		// add width, update before the next iteration
       		accW += (w + 0);
