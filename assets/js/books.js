87506e26df5dc8c61ed5620c131917095b555836
James Carey <57900176+carey-james@users.noreply.github.com>
2024-12-31 14:37:34 -0500

    Update books.js

--- assets/js/books.js
+++ assets/js/books.js
@@ -319,7 +319,7 @@ function runner(book_data) {
         		//update count for the previous values
         		d3.select(`#legend-0-${labelCounts[0] - 1}`).text(counts[0]);
         		runningCounts.forEach(function(value, key){
-        			console.log(`key : ${key}, counts2 : ${counts[2]}`)
+        			console.log(`key : ${key}, counts2 : ${counts[2]}`);
         			if ((key < counts[2]) && (counts[2] != 0)) {
         				let oldCount = parseInt(d3.select(`#legend-0-${labelCounts[0] - 2}`).text());
         				d3.select(`#legend-1-percent-${key}`).text(`${((value / oldCount) * 100).toString().split('.')[0]}%`);
@@ -346,12 +346,14 @@ function runner(book_data) {
         		d3.select(`#legend-1-${labelCounts[1] - 1}`).text(counts[1] + 1);
         		runningCounts.set(labelCounts[1] - 1, counts[1]);
         		runningCounts.forEach(function(value, key){
-        			console.log(`key : ${key}, counts2 : ${counts[2]}`)
+        			console.log(`key : ${key}, counts2 : ${counts[2]}`);
         			if ((key < counts[2]) && (counts[2] != 0)) {
         				let oldCount = parseInt(d3.select(`#legend-0-${labelCounts[0] - 2}`).text());
         				d3.select(`#legend-1-percent-${key}`).text(`${((value / oldCount) * 100).toString().split('.')[0]}%`);
+        				console.log(`${((value / oldCount) * 100).toString().split('.')[0]}%`);
         			} else {
         				d3.select(`#legend-1-percent-${key}`).text(`${((value / counts[0]) * 100).toString().split('.')[0]}%`);
+        				console.log(`${((value / counts[0]) * 100).toString().split('.')[0]}%`);
         			}	
         		})
       		}
