3851c4c514631f7ed1e164091b9e0d489769f85c
James Carey <57900176+carey-james@users.noreply.github.com>
2025-03-17 15:00:30 -0400

    Update games.js

--- assets/js/games.js
+++ assets/js/games.js
@@ -11,7 +11,7 @@ function runner(games_data, feedback_data) {
 		const filtered_items = summary_data.filter(item => item.game === game);
 		let summary_item = {};
 		summary_item.game = game;
-		summary_item.avg_mech_rating = parseFloat((filtered_items.map(item => parseInt(item.mechanics_enjoyment, 10)).reduce((sum, rating) => sum + rating, 0) / ratings.length).toFixed(1));
+		summary_item.avg_mech_rating = parseFloat((filtered_items.map(item => parseInt(item.mechanics_enjoyment, 10)).reduce((sum, rating) => sum + rating, 0) / filtered_items.length).toFixed(1));
 		summary_data.push(summary_item);
 	});
 
