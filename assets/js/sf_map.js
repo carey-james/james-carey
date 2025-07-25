e6d0e015d6abc348aff02c461825f9d3cdda9239
James Carey <57900176+carey-james@users.noreply.github.com>
2025-07-25 09:32:37 -0400

    Updating to CSV for SF

--- assets/js/sf_map.js
+++ assets/js/sf_map.js
@@ -14,7 +14,7 @@ async function initMap() {
 
   // Get the data on recs from the JSON file held in '/assets/data/'
   // And build the markers based on that
-  const recs = await d3.json("/assets/data/sf_recs.json");
+  const recs = await d3.dsv('|', '/assets/data/sf_recs.csv');
   for (const rec of recs) {
     const marker = new google.maps.marker.AdvancedMarkerElement({
       map,
