0f67476964812b5b44f9a914cdefceee11f222b8
James Carey <57900176+carey-james@users.noreply.github.com>
2026-08-15 14:04:54 -0400

    Working on rewrite to React

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+function App() {
+	return (
+		<>
+			<section id='header'>
+				<header className='major'>
+					<h1>James Carey</h1>
+					<h3>
+						<a href='./about.html'>about me</a>
+					</h3>
+					<ul className='icons-grid'>
+						<li>
+							<a href='./dc_map.html'>
+								<span className='icon major fa-solid fa-plate-utensils'></span>
+								<h3>DC Food & Drink Recs</h3>
+							</a>
+						</li>
+						<li>
+							<a href='/.sf_map.html'>
+								<span className='icon major fa-solid fa-signs-post'></span>
+								<h3>SF Bay Area Recs</h3>
+							</a>
+						</li>
+						<li>
+							<a href='/.books.html'>
+								<span className='icon major fa-solid fa-books'></span>
+								<h3>Bookshelf</h3>
+							</a>
+						</li>
+						<li>
+							<a href='/.games.html'>
+								<span className='icon major fa-solid fa-chess'></span>
+								<h3>Game Library</h3>
+							</a>
+						</li>
+					</ul>
+				</header>
+			</section>
+		</>
+	)
+}
+
+export default App
